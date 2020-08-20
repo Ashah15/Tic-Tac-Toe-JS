4eaa68cf178fa33f9deb578b4e366ae474426303
Ashah15 <ashaabdillahi53@gmail.com>
2020-08-20 22:56:19 +0300

    Create Player Factory

--- main.js
+++ main.js
@@ -85,3 +85,22 @@ const UI = (() => {
 		resetDOM,
 	};
 })();
+
+const playerFactory = (playerName, playerSymbol) => {
+    const name = playerName;
+    const symbol = playerSymbol;
+    let playerMoves = [];
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const getPlayerMoves = () => playerMoves;
+    const addMove = (move) => playerMoves.push(move);
+    const resetPlayerMoves = () => (playerMoves = []);
+    return {
+        getName,
+        getSymbol,
+        getPlayerMoves,
+        addMove,
+        resetPlayerMoves,
+    };
+};
+
