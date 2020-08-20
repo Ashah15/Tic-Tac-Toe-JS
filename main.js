f3ff3ba09335fe31fb843dd41fcfecb94d297030
Ashah15 <ashaabdillahi53@gmail.com>
2020-08-20 22:52:27 +0300

    Add logic module

--- main.js
+++ main.js
@@ -15,3 +15,33 @@ const gameBoard = (() => {
 		resetBoard,
 	};
 })();
+
+// Logic Module
+const logic = (() => {
+    const winningCombinations = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+    ];
+    const checkMove = (index, board) => board[index] === null;
+    const checkWin = (playerMoves) => {
+        let win = false;
+        for (let i = 0; i < winningCombinations.length; i++) {
+            let set = winningCombinations[i];
+            win = set.every((num) => playerMoves.includes(num));
+            if (win) break;
+        }
+        return win;
+    };
+    const checkTie = (board) => !board.includes(null);
+    return {
+        checkMove,
+        checkWin,
+        checkTie,
+    };
+})();
