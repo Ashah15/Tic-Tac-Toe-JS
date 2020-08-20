00f8c3c20117a46be1a933ccfd5aec8bcae711b3
Julian Blasco <blascoblasco9@gmail.com>
2020-08-20 16:54:32 -0300

    Update index.html and create UI module

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ const gameBoard = (() => {
 	};
 })();
 
-// Logic Module
 const logic = (() => {
     const winningCombinations = [
         [1, 2, 3],
@@ -44,4 +43,45 @@ const logic = (() => {
         checkWin,
         checkTie,
     };
+})();
+
+const UI = (() => {
+	const messageContainer = document.querySelector('.system-message');
+	const boxes = document.querySelectorAll('.grid-item');
+
+	const updateBox = (index, symbol) => (boxes[index].innerHTML = symbol);
+
+	const showWelcomeMessage = () => {
+		messageContainer.innerHTML = 'Welcome to the Tic Tac Toe game!';
+	};
+
+	const showEndMessage = (status, playerName) => {
+		let msg = 'Game Over. ';
+
+		status === 'Win' ? (msg += `Player ${playerName} Won!`) : (msg += 'It was a Tie!');
+
+		messageContainer.innerHTML = msg;
+	};
+
+	const showInvalidMoveMessage = () => {
+		messageContainer.innerHTML = 'Invalid move. Try again';
+	};
+
+	const showPlayerTurnMessage = (name) => {
+		messageContainer.innerHTML = `It's your turn player ${name}, make a move`;
+	};
+
+	const resetDOM = () => {
+		messageContainer.innerHTML = '';
+		boxes.forEach((box) => (box.innerHTML = ''));
+	};
+
+	return {
+		updateBox,
+		showWelcomeMessage,
+		showEndMessage,
+		showInvalidMoveMessage,
+		showPlayerTurnMessage,
+		resetDOM,
+	};
 })();
