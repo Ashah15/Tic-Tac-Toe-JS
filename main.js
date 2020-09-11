c8a8240267cbc802199509b03c2cc47bfa70832c
Ashah15 <ashaabdillahi53@gmail.com>
2020-09-11 20:26:13 +0300

    Modify files

--- main.js
+++ main.js
@@ -1,41 +1,7 @@
-// import domContent from './dom'
-// import logicContent from './logic'
-
-// let domContentInstance = domContent()
-// domContentInstance.UI()
-
-// let logicContentInstance = logicContent()
-// logicContentInstance.logic()
-
-// let domContent = [];
-// let gameBoard = [];
-// const playersArray = [];
-// let playerOneMoves = [];
-// let playerTwoMoves = [];
-// let newArr = [];
-
-
-// domContent.updateBox()
-// domContent.showWelcomeMessage()
-// domContent.showEndMessage()
-// domContent.showInvalidMoveMessage()
-// domContent.showPlayerTurnMessage()
-// domContent.resetDOM()
-// domContent.setPlayers()
-// domContent.gamePlayers()
-// domContent.buttons()
-// domContent.window.onload()
-
-// logicContent.Players()
-// logicContent.gameBoard()
-// logicContent.logic()
-// logicContent.playerFactory()
-// logicContent.game()
-
-const playersArray = [];
 const Players = (e) => {
   const playerOne = document.getElementById('player-one-input').value;
   const playerTwo = document.getElementById('player-two-input').value;
+  const playersArray = [];
   playersArray.push(playerOne);
   playersArray.push(playerTwo);
 
