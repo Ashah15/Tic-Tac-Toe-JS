e928ac0d78e29ca1871274bb36a0e489e2f3e068
Ashah15 <ashaabdillahi53@gmail.com>
2020-10-20 21:09:37 +0300

    performed tests on the UI.js file

--- __mocks__/UI.js
+++ __mocks__/UI.js
@@ -1,9 +1,12 @@
 const UI = (() => {
   const messageContainer = {innerHTML: ''}
-  const boxes = document.querySelectorAll('.grid-item');
+  const boxes = [{innerHTML: ''},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: ''},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: ''},{innerHTML: ''},{innerHTML: ''}]
 
   const updateBox = (index, symbol) => {
     (boxes[index].innerHTML = symbol);
+    return boxes
   };
 
   const showWelcomeMessage = () => {
@@ -17,13 +20,17 @@ const UI = (() => {
     status === 'Win' ? msg += `Player ${playerName} Won!` : msg += 'It was a Tie!'; // eslint-disable-line no-unused-expressions
 
     messageContainer.innerHTML = msg;
+    return messageContainer.innerHTML
   };
+
   const showInvalidMoveMessage = () => {
     messageContainer.innerHTML = 'Invalid move. Try again';
+    return messageContainer.innerHTML
   };
 
   const showPlayerTurnMessage = (name) => {
     messageContainer.innerHTML = `It's your turn player ${name}, make a move`;
+    return messageContainer.innerHTML
   };
 
   const resetDOM = () => {
@@ -31,6 +38,7 @@ const UI = (() => {
     boxes.forEach((box) => {
       box.innerHTML = '';
     });
+    return boxes
   };
 
   return {
--- tests/UI.test.js
+++ tests/UI.test.js
@@ -2,15 +2,45 @@ jest.mock('../UI')
 const { UI } = require('../UI');
 
 describe ('UI', () => {
-	it('returns welcome to the Tic Tac Toe game', () => {
-		// const message = jest.fn((selector) => {
-		// 	if (selector === '.system-message'){
-		// 	return {
-		// 		innerHTML: ''
-		// 	}
-		// }
-		// })
-		// Object.defineProperty(global.document, 'querySelector', { value: message});
-    expect(UI.showWelcomeMessage()).toBe('Welcome to the Tic Tac Toe game!');
-  });
+	describe ('showWelcomeMessage', () => {
+	  it('returns welcome to the Tic Tac Toe game', () => {
+      expect(UI.showWelcomeMessage()).toBe('Welcome to the Tic Tac Toe game!');
+      });
+    })
+
+    describe ('showInvalidMoveMessage', () => {
+	  it('returns Invalid move', () => {
+      expect(UI.showInvalidMoveMessage()).toBe('Invalid move. Try again');
+      });
+    })
+
+    describe ('showPlayerTurnMessage', () => {
+	  it('returns Its your turn player ${name}, make a move', () => {
+      expect(UI.showPlayerTurnMessage('Moin')).toBe(`It's your turn player Moin, make a move`);
+      });
+    })
+
+    describe ('showEndMessage', () => {
+	  it('returns end message of the game', () => {
+      expect(UI.showEndMessage('Win', 'Moin')).toBe('Game Over. Player Moin Won!');
+      });
+    })
+
+    describe ('resetDOM', () => {
+	  it('resets the dom', () => {
+      expect(UI.resetDOM()).toEqual([{innerHTML: ''},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: ''},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: ''},{innerHTML: ''},{innerHTML: ''}]);
+      });
+    })
+
+
+    describe ('updateBox', () => {
+	  it('returns the updated box', () => {
+      expect(UI.updateBox(3, 'O')).toEqual([{innerHTML: ''},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: 'O'},{innerHTML: ''},{innerHTML: ''},
+                 {innerHTML: ''},{innerHTML: ''},{innerHTML: ''}]);
+      });
+    })
+
 })
